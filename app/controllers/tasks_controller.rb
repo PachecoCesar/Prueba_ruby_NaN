9fe79f9c15bc00c96a27ca93b0cda35ae352d330
PachecoCesar <c86pacheco@gmail.com>
2019-09-30 10:51:04 -0300

    adding controllers

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,12 @@
 class TasksController < ApplicationController
+  before_action :authenticate_user!, only: :show
+
   def index
+    @tasks = Task.all
+
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 end
--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,20 @@
 class TodosController < ApplicationController
+  before_action :authenticate_user!
+    def index
+      @todos = current_user.todos
+    end
+
+
+    def update
+      todo = Todo.find(params[:todo_id])
+      todo.completed = !todo.completed
+      todo.save
+      redirect_to todos_path
+    end
+
+    def show
+      todo = Todo.find(params[:todo_id])
+      @task = todo.task
+      @todos = Todo.where(task_id: @task.id, completed: true)
+    end
 end
