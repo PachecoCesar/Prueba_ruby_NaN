a6c6ea5d240482645d829de3b12f316e1d0ca175
PachecoCesar <c86pacheco@gmail.com>
2019-09-30 10:39:18 -0300

    controllers added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+Task.destroy_all
+
+  9.times do |i|
+    Task.create(
+      title: Faker::GreekPhilosophers.name,
+      photo: "https://picsum.photos/id/#{i+11}/500/500"
+    )
+  end
