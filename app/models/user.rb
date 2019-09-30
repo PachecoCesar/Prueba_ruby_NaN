a44d7873e7059eba33e4e9fc4d06f4de7151e11c
PachecoCesar <c86pacheco@gmail.com>
2019-09-30 10:45:49 -0300

    model relationship added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  after_create :load_tasks
+
+  has_many :todos, dependent: :destroy
+  has_many :tasks, through: :todos
+
+
+  def load_tasks
+    Task.all.each do |task|
+      Todo.create(
+      task_id: task.id,
+      user_id: self.id
+      )
+    end
+  end
 end
